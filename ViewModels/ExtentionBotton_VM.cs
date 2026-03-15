4bff9399ad5a834d39bdbc82ff4e38715b00099d
Nico Zuber <zuber.nico@gmail.com>
2026-03-15 11:14:57 -0400

    Added seperate button control for extention filters.

---
+++ ViewModels/ExtentionBotton_VM.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using TagExplorer.Data;
+using TagExplorer.Models;
+
+namespace TagExplorer.ViewModels;
+
+public partial class ExtentionButton_VM : ObservableObject
+{
+    public event EventHandler FileTypeSelected;
+
+    [ObservableProperty] private FileType _fileType;
+    [ObservableProperty] private bool _isSelected;
+
+    public ExtentionButton_VM() { }
+
+    public ExtentionButton_VM(FileType fileType)
+    {
+        FileType = fileType;
+    }
+
+    [RelayCommand]
+    public void SelectFileType()
+    {
+        IsSelected = !IsSelected;
+        FileTypeSelected?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ Views/ExtentionButton_V.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TagExplorer.Views
+{
+    /// <summary>
+    /// Interaction logic for ExtentionBotton_V.xaml
+    /// </summary>
+    public partial class ExtentionButton_V : UserControl
+    {
+        public ExtentionButton_V()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
